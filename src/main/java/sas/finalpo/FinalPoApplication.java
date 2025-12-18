55676e5fdbcb4cec51cd1ece8d16ca9d241276be
adiletdzholdasbai <adilet.dzholdasbai@narxoz.kz>
2025-12-18 14:41:58 +0500

    first commit

---
+++ src/main/java/sas/finalpo/FinalPoApplication.java
@@ -0,0 +1,13 @@
+package sas.finalpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalPoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalPoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sas/finalpo/FinalPoApplicationTests.java
@@ -0,0 +1,13 @@
+package sas.finalpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalPoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
